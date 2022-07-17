1797dc66a827dcffc37aa2aebec38411aae4930c
abdelmoubine <abdelmoubine2@gmail.com>
2022-07-17 13:42:13 +0300

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+week = {
+    0 : "Sunday",
+    1 : "Monday",
+    2 : "Tuesday",
+    3 : "Wednesday",
+    4 : "Thyrsday",
+    5 : "Friday",
+    6 : "Saturday"
+}
+
+print(week)
+print(week[2])
+print(week.get(0, "Please try again"))
