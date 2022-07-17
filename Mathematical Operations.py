8b73792d8407efe2f8ce1f873acb3d8e011cd921
abdelmoubine <abdelmoubine2@gmail.com>
2022-07-17 13:40:37 +0300

    Mathematical Operations

---
+++ Mathematical Operations.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter Number1: "))
+num2 = int(input("Enter number2: "))
+
+resulat = num1 + num2
+print(resulat)
+
+##
+num1 = input("Enter Number1: ")
+num2 = input("Enter number2: ")
+
+resulat = int(num1) + int(num2)
+print(resulat)
+
+##
+num1 = input("Enter Number1: ")
+num2 = input("Enter number2: ")
+
+a = int(num1)
+b = int(num2)
+resulat = a + b
+print(resulat)
