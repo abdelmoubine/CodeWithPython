7a550d63c32e3551995ffd5f51773e9fcf1cb442
Abdel Moubine MISSAOUI <59146348+abdelmoubine@users.noreply.github.com>
2022-07-17 15:56:52 +0300

    Game (Math)

---
+++ Game - Tip Calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+bill_tip = bill * (1 + tip/100)
+bill_persone = round((bill_tip / people),2)
+print(f"Each Person Should Pay : ${bill_persone}")
