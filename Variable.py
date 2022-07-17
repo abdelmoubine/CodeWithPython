2f733b26d2b5bad93873ba2ed27846d769b07e99
abdelmoubine <abdelmoubine2@gmail.com>
2022-07-17 13:33:41 +0300

    Variable

---
+++ Variable.py
@@ -0,0 +1,6 @@
+name = "Abdel Moubine"
+age = "24"
+country = "Tunisia"
+live = "Moscow"
+
+print("Hello my name is " + name + ", i'am " + age + " years old, i'am from " + country + ", i live in " + live)
